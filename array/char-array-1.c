2af81dec06e02f1be7d6259c183f84cb0f6ae730
kjaehoon <k01067800594@gmail.com>
2026-03-30 14:44:44 +0900

    2차열배열

---
+++ array/char-array-1.c
@@ -0,0 +1,18 @@
+﻿// 문자열 ppt -> 5번 7번 9번 슬라이드
+// 문자열의 길이를 구하는 프로그램
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+    char str[30] = "C language";    //문자배열 30자
+    int i = 0;
+
+    while (str[i] != '\0')  //만족할때 반복
+                            //!= 다르다 \0는 null 값을 의미
+                            //문자를 내용 끝나는시점 반드시 null이 들어감
+        i++;
+
+    printf("문자열\"%s\"의 길이는 %d입니다.\n", str, i);
+    return 0;
+}
---
+++ array/char-array-2.c
@@ -0,0 +1,18 @@
+﻿// 과일 이름 3개이며 각 과일이름은 최대 20자
+#define _CRT_SECURE_NO_WARNINGS   
+#include <stdio.h>
+int main(void)
+{
+    int i;
+    char fruits[3][20];
+
+    for (i = 0; i < 3; i++)
+    {
+        printf("과일 이름 입력:");
+        scanf("%s", fruits[i]);
+        printf("%d 번째 과일: %s \n", i, fruits[i]);
+
+    }
+
+    return 0;
+}

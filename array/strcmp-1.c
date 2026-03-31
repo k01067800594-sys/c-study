2bf85a58017faa48c909be2a0dbeba9d07359c8d
kjaehoon <k01067800594@gmail.com>
2026-03-31 11:48:26 +0900

    문자열처리

---
+++ array/strcmp-1.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int res;
+    char a[30], b[30];
+    printf("첫번째 단어 입력:");
+    scanf("%s", a);
+    //문자열을 하나 통으로 할때는 &를 안붙이지만 그중하나만 정하여 값을 넣을땐 &를 사용한다
+    printf("두번째 단어 입력:");
+    scanf("%s", b);
+    res = strcmp(a, b);   //문자열비교
+    //res는 0, 음수, 양수
+    if (res < 0)    //음수이면 문자열1이 문자열2보다 사전순으로 앞에 있음
+    {
+        printf("%s가 %s보다 앞에 있다.\n", a, b);
+    }
+    else if (res == 0)  //반환값이 0이면 완전히 같다
+    {
+        printf("%s와 %s가 같다.", a, b);
+    }
+    else
+    {
+        printf("%s가 %s보다 작다.", a, b);
+        //양수이면 문자열1이 문자열2보다 사전순으로 뒤에 있음
+    }
+
+    return 0;
+}
---
+++ array/strcpy.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>  //#include 전처리기. 입출력 관련 헤더파일
+#include <string.h>
+
+int main(void)
+{
+    char a[10] = { "hello" };
+    printf("%s\n", a);
+    printf("%c\n", a[0]);
+
+    char b[10];
+    //b = "love";   오류
+    strcpy(b, "love");  //문자열복사(대상, 문자열)
+    /*b[0] = 'l';
+    b[1] = 'o';
+    b[2] = 'v';
+    b[3] = 'e';*/
+    printf("%s", b);
+    //strcpy : 문자배열을 선언이후 뒤에 최기화가 암됨
+              //이후 문자열 복사할때
+
+
+    return 0;
+}

8d1b74d99907c02e9edf033f8cf5a055f6d7ff4f
kjaehoon <k01067800594@gmail.com>
2026-04-06 15:50:13 +0900

    더블 포인터 실습

---
+++ pointer/double point-2.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+
+//void change(int* p)
+//{
+//    int b = 20;
+//    p = &b;
+//}
+//int main(void)
+//{
+//    int a = 10;
+//    int* p = &a;
+//
+//    change(p);  //포인터여도 주소값 복사(값 전달, call by value)
+//
+//    printf("%d\n", *p);
+//
+//
+//
+//    return 0;
+//}
+
+void change(int** pp)
+{
+    int b = 20;
+    *pp = &b;
+}
+int main(void)
+{
+    int a = 10;
+    int* p = &a;
+
+    change(&p);  //포인터여도 주소값 복사(값 전달, call by value)
+
+    printf("%d\n", *p);
+
+
+
+    return 0;
+}
